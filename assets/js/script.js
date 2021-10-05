38e43dce42e4bbde07388b93198e9a4f5048212d
Junior da Silva <jr.a.silva@outlook.com>
2021-10-05 08:47:54 -0300

    Inicial

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,22 +50,22 @@
 
 
 /* JUNIOR */
+function whoseTurnIsIt () {
+    if (whoPlayed === 'red') {
+        isRedTurn = false;
+        isBlackTurn = true;
+    } 
+    else if (whoPlayed === 'black') {
+        isRedTurn = true;
+        isBlackTurn = false;        
+    }    
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* global variables needed */
+let whoPlayed = '';
+let isRedTurn = false;
+let isBlackTurn = false;
+/* ---------- */
 
 
 
