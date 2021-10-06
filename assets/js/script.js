226f80e7c0d9b59d559c62a82664ee7da914b9dc
Vitor Lins <vitorgblins@gmail.com>
2021-10-06 15:59:33 -0300

    commit vitor Function msg vitoria e function addCircle

--- assets/js/script.js
+++ assets/js/script.js
@@ -139,12 +139,51 @@ generateTable()
 /* VITOR */
 
 
+/* FUNCTION QUE RETORNA MENSAGEM DE VITORIA PARA O GANHADOR */ 
 
+function victoryMessage (conditionV, conditionH, conditionD, player){
+    if(conditionV || conditionH || conditionD){
+        alert(`${player} venceu!`)
+    }
+    
+}
 
+/* Função para adicionar círculos na posição correta */
 
+    //constante para pegar tabela inteira
+    const tabela = document.getElementById('game')
 
+    // evento de clique na tabela
+    tabela.addEventListener('click', function (event){
 
+      let col = event.target.parentElement
+      console.log(col) 
+        addCircle(col, player)
+      
+})
+const player = 'red'
 
+function addCircle(col, player) {
+        
+        const circle = document.createElement('div')
+        circle.classList.add('circles')
+        circle.classList.add(player)
+
+        // transformo parametro col em array para tratar seus elementos
+        const newCol = col.children
+
+        //laço for para identificar o espaço a ser ocupado pela bolinha
+        for (let i = newCol.length -1; i >= 0; i--){
+            
+            console.log(newCol[i])
+            if(newCol[i].innerHTML === ''){
+
+                newCol[i].appendChild(circle)
+   
+                i = -1 
+            }       
+        }
+}
 
 
 
