9716d1d1916686c47a64596eea77257f4b3b5fd5
André Martins <andresmartins910@gmail.com>
2021-10-06 10:40:18 -0300

    Função para validar verticalmente.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// let whoPlayed = 'red';
+
 /* ANDRÉ */
 function generateTable() {
     const main = document.createElement('main')
@@ -28,23 +30,76 @@ function generateTable() {
 
 generateTable()
 
+// const divContainerGroup = document.querySelector('#game');
+// divContainerGroup.addEventListener('click', event => {
+//     const column = event.target.parentElement.children;
+//     addCircle(column, whoPlayed);
+//     whoseTurnIsIt(whoPlayed);
+// });
 
 
+// function whoseTurnIsIt(currentPlayer) {
+//     if (currentPlayer === 'red') {
+//         whoPlayed = 'black';
+//     } else if (currentPlayer === 'black') {
+//         whoPlayed = 'red';
+//     }
+// }
+// const addCircle = (columnArray, currentPlayer) => {
+//     const disc = document.createElement('div');
+//     disc.classList.add('disc');
+//     if (currentPlayer === 'red') {
+//         disc.classList.add('red');
+//     } else if (currentPlayer === 'black') {
+//         disc.classList.add('black');
+//     }
+//     for (let i = columnArray.length - 1; i >= 0; i--) {
+//         const currentCell = columnArray[i];
+//         if (currentCell.firstChild === null) {
+//             console.log(currentCell.innerHTML);
+//             currentCell.appendChild(disc);
+//             i = -1;
+//         }
+//     }
+// };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function validateVertical() {
+    for (let j = 1; j <= 6; j++) {
+        const cells = document.querySelector(`#column${j}`).children
+    
+        let discArray = []
+        for (let i = 0; i < cells.length; i++) {
+            let discs = cells[i].firstChild
+            if (discs !== null) {
+                discArray.push(discs.getAttribute('class'))
+            }
+        }
+    
+        let blackCounter = 0
+        let redCounter = 0
+    
+        for (let i = 0; i < discArray.length; i++) {
+            if (columns[i] === 'disc black') {
+                blackCounter++
+                if (discArray[i + 1] === 'disc red') {
+                    redCounter = 0
+                }
+            }
+            if (discArray[i] === 'disc red') {
+                redCounter++
+                if (discArray[i + 1] === 'disc black') {
+                    blackCounter = 0
+                }
+            }
+        }
+        if (blackCounter === 4) {
+            return true
+        } else if (redCounter === 4) {
+            return true
+        }
+    }
+    return false
+}
 
 
 
