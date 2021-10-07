c2f1c3da6840490b32b8e9af4d0bdd131ed16907
Rafael Carvalho <rafaeloc.dev@gmail.com>
2021-10-07 06:42:19 -0300

    Reordered the functions and renamed some variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,307 +1,139 @@
+generateTable();
+const board = document.getElementById('game');
+const restartButton = document.getElementById('restartButton');
 let whoPlayed = 'red';
 
-/* ANDRÉ */
 function generateTable() {
-    const main = document.createElement('main')
-    main.id = 'game'
-    document.body.appendChild(main)
+  const main = document.createElement('main');
+  main.id = 'game';
+  document.body.appendChild(main);
 
-    // generate columns
-    let counterColumns = 1
-    for (let i = 1; i <= 7; i++) {
-        const divColumn = document.createElement('div')
-        divColumn.className = 'columns'
-        divColumn.id = `column${counterColumns++}`
-        main.appendChild(divColumn)
+  // generate columns
+  let counterColumns = 1;
+  for (let i = 1; i <= 7; i++) {
+    const divColumn = document.createElement('div');
+    divColumn.className = 'columns';
+    divColumn.id = `column${counterColumns++}`;
+    main.appendChild(divColumn);
+  }
+
+  // generate cells
+  const columns = document.querySelectorAll('.columns');
+  let counterCells = 1;
+  columns.forEach(column => {
+    for (let i = 1; i <= 6; i++) {
+      const divCells = document.createElement('div');
+      divCells.className = 'cells';
+      divCells.id = `cell${counterCells++}`;
+      column.appendChild(divCells);
     }
-
-    // generate cells
-    const columns = document.querySelectorAll('.columns')
-    let counterCells = 1
-    columns.forEach(column => {
-        for (let i = 1; i <= 6; i++) {
-            const divCells = document.createElement('div')
-            divCells.className = 'cells'
-            divCells.id = `cell${counterCells++}`
-            column.appendChild(divCells)
-        }
-    })
+  });
 }
 
-generateTable()
+function addCircle(col, currentPlayer) {
+  const circle = document.createElement('div');
+  circle.classList.add('disc');
+  circle.classList.add(currentPlayer);
 
+  const newCol = col.children;
 
+  for (let i = newCol.length - 1; i >= 0; i--) {
+    if (newCol[i].innerHTML === '') {
+      newCol[i].appendChild(circle);
+      i = -1;
+    }
+  }
+}
 
 function whoseTurnIsIt(currentPlayer) {
-    if (currentPlayer === 'red') {
-        whoPlayed = 'black';
-    } else if (currentPlayer === 'black') {
-        whoPlayed = 'red';
-    }
+  if (currentPlayer === 'red') {
+    whoPlayed = 'black';
+  } else if (currentPlayer === 'black') {
+    whoPlayed = 'red';
+  }
 }
-const addCircle = (columnArray, currentPlayer) => {
-    const disc = document.createElement('div');
-    disc.classList.add('disc');
-    if (currentPlayer === 'red') {
-        disc.classList.add('red');
-    } else if (currentPlayer === 'black') {
-        disc.classList.add('black');
-    }
-    for (let i = columnArray.length - 1; i >= 0; i--) {
-        const currentCell = columnArray[i];
-        if (currentCell.firstChild === null) {
-            console.log(currentCell.innerHTML);
-            currentCell.appendChild(disc);
-            i = -1;
-        }
-    }
-};
 
 function validateVertical() {
-    for (let j = 1; j <= 6; j++) {
-        const cells = document.querySelector(`#column${j}`).children
+  for (let j = 1; j <= 6; j++) {
+    const cells = document.querySelector(`#column${j}`).children;
 
-        let discArray = []
-        for (let i = 0; i < cells.length; i++) {
-            let discs = cells[i].firstChild
-            if (discs !== null) {
-                discArray.push(discs.getAttribute('class'))
-            }
-        }
-
-        let blackCounter = 0
-        let redCounter = 0
-
-        for (let i = 0; i < discArray.length; i++) {
-            if (discArray[i] === 'disc black') {
-                blackCounter++
-                if (discArray[i + 1] === 'disc red') {
-                    redCounter = 0
-                }
-            }
-            if (discArray[i] === 'disc red') {
-                redCounter++
-                if (discArray[i + 1] === 'disc black') {
-                    blackCounter = 0
-                }
-            }
-        }
-        if (blackCounter === 4) {
-            return true
-        } else if (redCounter === 4) {
-            return true
-        }
+    let discArray = [];
+    for (let i = 0; i < cells.length; i++) {
+      let discs = cells[i].firstChild;
+      if (discs !== null) {
+        discArray.push(discs.getAttribute('class'));
+      }
     }
-    return false
+
+    let blackCounter = 0;
+    let redCounter = 0;
+
+    for (let i = 0; i < discArray.length; i++) {
+      if (discArray[i] === 'disc black') {
+        blackCounter++;
+        if (discArray[i + 1] === 'disc red') {
+          redCounter = 0;
+        }
+      }
+      if (discArray[i] === 'disc red') {
+        redCounter++;
+        if (discArray[i + 1] === 'disc black') {
+          blackCounter = 0;
+        }
+      }
+    }
+    if (blackCounter === 4) {
+      return true;
+    } else if (redCounter === 4) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function validateHorizontal() {
-    for (let x = 1; x <= 6; x++) {
-        let discArray = [];
-        for (let i = x; i <= x + 36; i += 6) {
-            const cells = document.querySelector(`#cell${i}`);
-            let discs = cells.firstChild;
-            if (discs !== null) {
-                discArray.push(discs.getAttribute('class'));
-            }
-        }
-        let blackCounter = 0;
-        let redCounter = 0;
-        for (let i = 0; i < discArray.length; i++) {
-            if (discArray[i] === 'disc black') {
-                blackCounter++;
-                if (discArray[i + 1] === 'disc red') {
-                    redCounter = 0;
-                }
-            }
-            if (discArray[i] === 'disc red') {
-                redCounter++;
-                if (discArray[i + 1] === 'disc black') {
-                    blackCounter = 0;
-                }
-            }
-        }
-        if (blackCounter === 4) {
-            return true;
-        } else if (redCounter === 4) {
-            return true;
-        }
+  for (let x = 1; x <= 6; x++) {
+    let discArray = [];
+    for (let i = x; i <= x + 36; i += 6) {
+      const cells = document.querySelector(`#cell${i}`);
+      let discs = cells.firstChild;
+      if (discs !== null) {
+        discArray.push(discs.getAttribute('class'));
+      }
     }
-    return false;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* JUNIOR */
-function whoseTurnIsIt (whoPlayed) {
-    if (whoPlayed === 'red') {
-       whoPlayed = 'black' ;
-    } 
-    else if (whoPlayed === 'black') {
-        whoPlayed = 'red' ;
-    }    
-}
-whoPlayed = 'red' ;
-
-const restartButton = document.getElementById("restartButton");
-
-function deleteTable() {
-    const deleteMainTable = document.getElementById('game');
-    deleteMainTable.remove();
-}
-
-function restart() {
-    whoPlayed = '';
-    deleteTable();
-    generateTable();
-}
-
-restartButton.onclick = function () {
-    restart();
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* VITOR */
-
-
-/* FUNCTION QUE RETORNA MENSAGEM DE VITORIA PARA O GANHADOR */ 
-
-function victoryMessage (conditionV, conditionH, conditionD, player){
-    if(conditionV || conditionH || conditionD){
-        alert(`${player} venceu!`)
-    }
-    
-}
-
-/* Função para adicionar círculos na posição correta */
-
-    //constante para pegar tabela inteira
-    const tabela = document.getElementById('game')
-
-    // evento de clique na tabela
-    tabela.addEventListener('click', function (event){
-
-      let col = event.target.parentElement
-      console.log(col) 
-        addCircle(col, player)
-      
-})
-const player = 'red'
-
-function addCircle(col, player) {
-        
-        const circle = document.createElement('div')
-        circle.classList.add('circles')
-        circle.classList.add(player)
-
-        // transformo parametro col em array para tratar seus elementos
-        const newCol = col.children
-
-        //laço for para identificar o espaço a ser ocupado pela bolinha
-        for (let i = newCol.length -1; i >= 0; i--){
-            
-            console.log(newCol[i])
-            if(newCol[i].innerHTML === ''){
-
-                newCol[i].appendChild(circle)
-   
-                i = -1 
-            }       
+    let blackCounter = 0;
+    let redCounter = 0;
+    for (let i = 0; i < discArray.length; i++) {
+      if (discArray[i] === 'disc black') {
+        blackCounter++;
+        if (discArray[i + 1] === 'disc red') {
+          redCounter = 0;
         }
+      }
+      if (discArray[i] === 'disc red') {
+        redCounter++;
+        if (discArray[i + 1] === 'disc black') {
+          blackCounter = 0;
+        }
+      }
+    }
+    if (blackCounter === 4) {
+      return true;
+    } else if (redCounter === 4) {
+      return true;
+    }
+  }
+  return false;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* RAFAEL */
-const diagonalValidation = divContainer => {
+function validateDiagonal(boardContainer) {
   // validation from left to right
   for (let j = 5; j >= 3; j--) {
     for (let i = 0; i <= 3; i++) {
       let row = j;
       const coloredCircleGroup = [];
       for (let column = i; column <= i + 3; column++) {
-        const coloredCircle = divContainer.children[column].children[row].firstChild;
+        const coloredCircle = boardContainer.children[column].children[row].firstChild;
         if (coloredCircle !== null) {
           coloredCircleGroup.push(coloredCircle.classList[1]);
         }
@@ -321,7 +153,7 @@ const diagonalValidation = divContainer => {
       let row = j;
       const coloredCircleGroup = [];
       for (let column = i; column >= i - 3; column--) {
-        const coloredCircle = divContainer.children[column].children[row].firstChild;
+        const coloredCircle = boardContainer.children[column].children[row].firstChild;
         if (coloredCircle !== null) {
           coloredCircleGroup.push(coloredCircle.classList[1]);
         }
@@ -335,18 +167,33 @@ const diagonalValidation = divContainer => {
     }
   }
   return false;
+}
+
+function deleteTable() {
+  const deleteMainTable = document.getElementById('game');
+  deleteMainTable.remove();
+}
+
+/* Necessita ser revista */
+function restart() {
+  whoPlayed = '';
+  deleteTable();
+  generateTable();
+}
+
+restartButton.onclick = function () {
+  restart();
 };
 
+function victoryMessage(conditionV, conditionH, conditionD, player) {
+  if (conditionV || conditionH || conditionD) {
+    alert(`${player} venceu!`);
+  }
+}
+
+board.addEventListener('click', function (event) {
+  const col = event.target.parentElement;
+  addCircle(col, whoPlayed);
 
 
-
-
-
-
-
-
-
-
-
-
-
+});
